73a5069000483b647fb872f6ecd06cdb10b79b9a
im_shadowpool <shadowpoolvs@gmail.com>
2025-09-29 18:59:48 +0530

    added page on project 2

---
+++ gsap_project_2/src/app/homepage/page.jsx
@@ -0,0 +1,35 @@
+const homepage = () => {
+  return (
+    <div className="bg-white">
+      <main className="homepage-main">
+        <section className="section-1 text-white text-[72px] flex items-center justify-center">
+          <div className="text-container ">
+            <h2>Section 1</h2>
+          </div>
+        </section>
+        <section className="section-2 text-black text-[72px] flex items-center justify-center">
+          <div className="text-container ">
+            <h2>Section 2</h2>
+          </div>
+        </section>
+        <section className="section-3 flex items-center justify-center">
+          <div className="text-container ">
+            <h2>Section 3</h2>
+          </div>
+        </section>
+        <section className="section-4 flex items-center justify-center">
+          <div className="text-container ">
+            <h2>Section 4</h2>
+          </div>
+        </section>
+        <section className="section-5 flex items-center justify-center">
+          <div className="text-container ">
+            <h2>Section 5</h2>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default homepage;
